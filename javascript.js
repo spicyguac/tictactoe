83c179c46351e051e0f2380de9f330c47654bd4b
spicyguac <200~46731258+spicyguac@users.noreply.github.com>
2025-01-20 17:12:20 -0600

    Add logic test to see if x or o won

--- javascript.js
+++ javascript.js
@@ -14,12 +14,43 @@ function newBoard(){
     };
 }
 
-function gameLogic(baord){
+function gameLogic(gameBoard){
 
+    let test = gameBoard
+
+    let xWon = [];
+    let oWon = [];
+    let xWonPerp = [];
+    let oWonPerp = [];
+
+    console.log(test);
+
+    for (let i = 0; i < test.length; i++) {
+        console.log(xWon);
+        for(let j = 0; j < test[i].length; j++) {
+            if(test[i][j] == "X"){
+                xWon.push(test[i][j]);
+            }else if(test[i][j] == "O"){
+                oWon.push(test[i][j]);
+            }
+
+            if(xWon.length > 0){
+                if(xWon[j - 1] != xWon[j]){
+                    xWon[j] = xWon[j-1];
+                    break;
+                }
+            }
+        }
+    }
 }
 
 function newPlayer(player){
+    let wins = 0;
+    
+    const getWins = () => wins;
+    const giveWin = () => wins++;
 
+    return {getWins, giveWin, wins};
 }
 
 function playGame(){
@@ -29,5 +60,7 @@ function playGame(){
 const new1 = newBoard();
 new1.addX(1,1);
 new1.addO(1,2);
+new1.addO(0,1);
+new1.addO(0,2);
 
 console.log(new1.board);
