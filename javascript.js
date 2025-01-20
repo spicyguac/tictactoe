8a83c5de8e0085248ae610d98283b822cb378ddc
spicyguac <200~46731258+spicyguac@users.noreply.github.com>
2025-01-20 12:03:48 -0600

    Create skeleton functions for game elements

--- javascript.js
+++ javascript.js
@@ -1,10 +1,33 @@
 
-function tttGame(){
+function newBoard(){
     let board = [
         [null, null, null],
-        [null, 1, null],
+        [null, null, null],
         [null, null, null]
     ];
 
+    const addX = (x,y) => board[x][y] = "X";
+    const addO = (x,y) => board[x][y] = "O";
+
+    return{
+        addX, addO, board
+    };
+}
+
+function gameLogic(baord){
+
+}
+
+function newPlayer(player){
+
+}
+
+function playGame(){
     
 }
+
+const new1 = newBoard();
+new1.addX(1,1);
+new1.addO(1,2);
+
+console.log(new1.board);
