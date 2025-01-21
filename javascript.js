7fb1cfbbf92c5eb7038308f5cea4f8bdb3f92ac1
spicyguac <200~46731258+spicyguac@users.noreply.github.com>
2025-01-20 19:00:50 -0600

    Add hasWon function to board object to see who won

--- javascript.js
+++ javascript.js
@@ -9,40 +9,90 @@ function newBoard(){
     const addX = (x,y) => board[x][y] = "X";
     const addO = (x,y) => board[x][y] = "O";
 
+    function hasWon(){
+        let xWin = 0;
+        let oWin = 0;
+        if(board[1][1] != null){
+            if(board[1][1] == "X"){
+                if(board[0][0] == "X"){
+                    if(board[2][2] == "X"){
+                        xWin = 1;
+                    }
+                }
+                if(board[0][2] == "X"){
+                    if(board[2][2] == "X"){
+                        xWin = 1;
+                    }
+                }
+            }else if(board[1][1] = "O"){
+
+                if(board[0][0] == "O"){
+                    if(board[2][2] == "O"){
+                        oWin = 1;
+                    }
+                }
+                if(board[0][2] == "O"){
+                    if(board[2][0] == "O"){
+                        oWin = 1;
+                    }
+                }
+            }
+        }
+
+        let xWonHoriz = [];
+        let oWonHoriz = [];
+        let xWonPerp = [];
+        let oWonPerp = [];
+    
+        if(xWin != 1 && oWin != 1){
+            for (let i = 0; i < board.length; i++) {
+                console.log(xWonHoriz);
+                if(xWonHoriz.length == 3 || xWonPerp.length == 3){
+                    console.log("X Win");
+                    xWin = 1;
+                    break;
+                }else if(oWonHoriz.length == 3 || oWonPerp == 3){
+                    console.log("O Win");
+                    oWin = 1;
+                    break;
+                }
+        
+                for(let j = 0; j < board[i].length; j++) {
+                    if(board[i][j] == "X"){
+                        xWonHoriz.push(board[i][j]);
+                    }else if(board[i][j] == "O"){
+                        oWonHoriz.push(board[i][j]);
+                    }
+        
+                    if(board[j][i] == "X"){
+                        xWonPerp.push(board[j][i]);
+                    }else if(board[j][i] = "O"){
+                        oWonPerp.push(board[j][i]);
+                    }
+                }
+            }
+        }
+    
+        if(xWin == 1){
+            return "X";
+        }else if(oWin == 1){
+            return "O";
+        }
+    }    
+
     return{
-        addX, addO, board
+        addX, addO, hasWon ,board
     };
 }
 
 function gameLogic(gameBoard){
+    console.log(gameBoard)
+    let board = gameBoard
 
-    let test = gameBoard
 
-    let xWon = [];
-    let oWon = [];
-    let xWonPerp = [];
-    let oWonPerp = [];
-
-    console.log(test);
-
-    for (let i = 0; i < test.length; i++) {
-        console.log(xWon);
-        for(let j = 0; j < test[i].length; j++) {
-            if(test[i][j] == "X"){
-                xWon.push(test[i][j]);
-            }else if(test[i][j] == "O"){
-                oWon.push(test[i][j]);
-            }
-
-            if(xWon.length > 0){
-                if(xWon[j - 1] != xWon[j]){
-                    xWon[j] = xWon[j-1];
-                    break;
-                }
-            }
-        }
-    }
 }
+
+
 
 function newPlayer(player){
     let wins = 0;
@@ -58,9 +108,6 @@ function playGame(){
 }
 
 const new1 = newBoard();
-new1.addX(1,1);
-new1.addO(1,2);
-new1.addO(0,1);
-new1.addO(0,2);
+
 
 console.log(new1.board);
